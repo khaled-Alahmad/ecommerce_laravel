4a0e4b0846bf769c93a1f65763ba1a8e4a708ff5
khaled-alahmad <aabboorroossee@gmail.com>
2022-09-05 12:20:54 +0300

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index(){
+        $product=Product::all();
+        $category=Category::all();
+        return view('view.index',[
+            'products'=>$product,
+            'categories'=>$category
+        ]);
+    }
+}
